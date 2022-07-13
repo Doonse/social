d334b451daf1eb4855ace3aaa4cc3ce8b2c1766b
Doonse <danielius.s2601@gmail.com>
2022-07-13 12:59:23 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router, Route, useNavigate, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const user = fetchUser();
+
+    if(!user) navigate('/login');
+  }, [])
+  
+
   return (
       <Routes>
         <Route path='login' element={<Login />} />
